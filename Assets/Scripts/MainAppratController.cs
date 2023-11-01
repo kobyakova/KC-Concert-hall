86db424719656d34f62aa466b70dffc51410be7c
iptnkt <125504831+iptnkt@users.noreply.github.com>
2023-11-01 16:42:22 +0300

    KCH-12-ApparatRoom

--- Assets/Scripts/MainAppratController.cs
+++ Assets/Scripts/MainAppratController.cs
@@ -5,8 +5,11 @@ using VRC.SDKBase;
 using VRC.Udon;
 using UnityEngine.UI;
 
+
+
 public class MainAppratController : UdonSharpBehaviour
 {
+    //Panles GROUP
     public UnityEngine.UI.Slider panelHue;
     public UnityEngine.UI.Slider panelSaturate;
     public UnityEngine.UI.Slider panelPower;
@@ -16,15 +19,35 @@ public class MainAppratController : UdonSharpBehaviour
     public Material[] panelMaterial;
     public GameObject[] panel;
 
+
+    //Projectors GROUP
+    public UnityEngine.UI.Slider projectorsHue;
+    public UnityEngine.UI.Slider projectorsSaturate;
+    public UnityEngine.UI.Slider projectorsPower;
+    [SerializeField]
+    public VRSL.VRStageLighting_AudioLink_Static[] projectors;
+
+    //PostPorc GROUP
+    public UnityEngine.UI.Slider postProcHue;
+    public UnityEngine.UI.Slider postProcSaturate;
+    public UnityEngine.UI.Slider postProcPower;
+    public GameObject postProc;
+
+    //Other GROUP
+
+
     void Start()
     {
         panelSaturate.value = 1;
         panelPower.value = 1;
         _panelModeChange();
-        _OnColorChange();
-        
+        _OnPanelColorChange();
+        _OnProjectorColorChange();
+        _OnPostProcChange();
+
+
     }
-    public void _OnColorChange()
+    public void _OnPanelColorChange()
     {
         float panHue = panelHue.value;
         float panSat = panelSaturate.value;
@@ -34,13 +57,13 @@ public class MainAppratController : UdonSharpBehaviour
 
         panCol *= Mathf.Pow(panPow, 3.3f);
         ActualMat.SetColor("_EmissionColor", panCol);
-        
+
     }
     public void _panelModeChange()
     {
         int panMod = panelLightMode.value;
         ActualMat = panelMaterial[panMod];
-        Debug.Log(panMod + "panMod");
+        
 
         for (int i = 0; i < panel.Length; i++)
         {
@@ -50,6 +73,38 @@ public class MainAppratController : UdonSharpBehaviour
             //Material[] mats =  panel[i].GetComponent<Renderer>().materials;
             //mats[].material.SetColor("_Color", panCol);
         }
-        _OnColorChange();
+        _OnPanelColorChange();
+    }
+    public void _OnProjectorColorChange()
+    {
+
+        float prjHue = projectorsHue.value;
+        float prjSat = projectorsSaturate.value;
+        float prjPow = projectorsPower.value;
+
+        Color prjCol = Color.HSVToRGB(prjHue, prjSat, 1);
+
+        prjCol *= Mathf.Pow(prjPow, 3.3f);
+
+        for (int i = 0; i < projectors.Length; i++)
+        {
+            projectors[i].LightColorTint = prjCol;
+        }
+        //ActualMat.SetColor("_EmissionColor", prjCol);
+    }
+
+    public void _OnPostProcChange()
+    {
+        float pPHue = postProcHue.value;
+        float pPSat = postProcSaturate.value;
+        float pPPow = postProcPower.value;
+
+        Material pPMat = postProc.GetComponent<Renderer>().material;
+
+        Color pPCol = Color.HSVToRGB(pPHue, pPSat, 1);
+        pPMat.SetColor("_EmissionColor", pPCol);
+        
+        pPMat.SetFloat("_Light", pPPow);
+        //Debug.Log(lightPower + "lightPower");
     }
 }
