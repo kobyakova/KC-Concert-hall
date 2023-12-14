911e8cd038184e4d033d855dd3c03442a9be92c4
iptnkt <iptnikitagc@gmail.com>
2023-12-14 12:06:57 +0300

    KCH-24-Dramology

--- Assets/Dreamology/Scripts/DreamologyAnims.cs
+++ Assets/Dreamology/Scripts/DreamologyAnims.cs
@@ -12,6 +12,7 @@ public class DreamologyAnims : UdonSharpBehaviour
     public GameObject[] effects;
     public Toggle[] effectToggles;
 
+    public Text currentActText;
     public string[] ActNames;
 
     private void Start()
@@ -20,48 +21,114 @@ public class DreamologyAnims : UdonSharpBehaviour
     }
     public void ActHall()
     {
-        dreamologiAnimator.Play(ActNames[0]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Hall");
     }
+    public void Hall()
+    {
+        dreamologiAnimator.Play(ActNames[0]);
+        currentActText.text = ActNames[0];
+    }
+
     public void ActHallBroken()
     {
-        dreamologiAnimator.Play(ActNames[1]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "HallBroken");
     }
+    public void HallBroken()
+    {
+        dreamologiAnimator.Play(ActNames[1]);
+        currentActText.text = ActNames[1];
+    }
+
     public void ActHallAppear()
     {
-        dreamologiAnimator.Play(ActNames[2]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "HallAppear");
     }
+    public void HallAppear()
+    {
+        dreamologiAnimator.Play(ActNames[2]);
+        currentActText.text = ActNames[2];
+    }
+
     public void ActWakeUp()
     {
-        dreamologiAnimator.Play(ActNames[3]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "WakeUp");
     }
+    public void WakeUp()
+    {
+        dreamologiAnimator.Play(ActNames[3]);
+        currentActText.text = ActNames[3];
+    }
+
     public void ActDiologs()
     {
-        dreamologiAnimator.Play(ActNames[4]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Diologs");
     }
+    public void Diologs()
+    {
+        dreamologiAnimator.Play(ActNames[4]);
+        currentActText.text = ActNames[4];
+    }
+
     public void ActArhetip()
     {
-        dreamologiAnimator.Play(ActNames[5]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Arhetip");
     }
+    public void Arhetip()
+    {
+        dreamologiAnimator.Play(ActNames[5]);
+        currentActText.text = ActNames[5];
+    }
+
     public void ActNoise()
     {
-        dreamologiAnimator.Play(ActNames[6]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Noise");
     }
+    public void Noise()
+    {
+        dreamologiAnimator.Play(ActNames[6]);
+        currentActText.text = ActNames[6];
+    }
+
     public void ActChoise()
     {
-        dreamologiAnimator.Play(ActNames[7]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Choise");
     }
+    public void Choise()
+    {
+        dreamologiAnimator.Play(ActNames[7]);
+        currentActText.text = ActNames[7];
+    }
+
     public void ActIdle()
     {
-        dreamologiAnimator.Play(ActNames[8]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Idle");
     }
+    public void Idle()
+    {
+        dreamologiAnimator.Play(ActNames[8]);
+        currentActText.text = ActNames[8];
+    }
+
     public void ActEnd1()
     {
-        dreamologiAnimator.Play(ActNames[9]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "End1");
     }
+    public void End1()
+    {
+        dreamologiAnimator.Play(ActNames[9]);
+        currentActText.text = ActNames[9];
+    }
+
     public void ActEnd2()
     {
-        dreamologiAnimator.Play(ActNames[10]);
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "End2");
     }
+    public void End2()
+    {
+        dreamologiAnimator.Play(ActNames[10]);
+        currentActText.text = ActNames[10];
+    }
+
 
     public void EffectsInclude()
     {
